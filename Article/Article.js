ecaa4c460efaba05e467d73f55a25e359975946e
unknown <Dustin.Guillen14@yahoo.com>
2020-01-15 14:39:43 -0700

    added imports, started article

--- Article/Article.js
+++ Article/Article.js
@@ -112,3 +112,11 @@ const data = [
   Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new article.
 
 */
+
+function createArticle(title, date, firstP, secondP, thirdP){
+
+  
+
+}
+
+
